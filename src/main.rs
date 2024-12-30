3f7c1c5f93ebc28344d950d1a3da863ccc7fb62b
Richard Zak <richard.j.zak@gmail.com>
2024-12-29 20:25:23 -0500

    refactor: don't use depricated event loop api
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod assets;
-use assets::load_icon;
+
+use std::cell::RefCell;
 
 use sysinfo::System;
 use tray_icon::menu::{AboutMetadata, Menu, MenuEvent, MenuItem, PredefinedMenuItem};
-use tray_icon::{TrayIconBuilder, TrayIconEvent};
-use winit::event_loop::EventLoop;
-use winit::{event::Event, event_loop::ControlFlow};
+use tray_icon::{TrayIcon, TrayIconBuilder, TrayIconEvent};
+use winit::application::ApplicationHandler;
+use winit::event::{StartCause, WindowEvent};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::WindowId;
 
 pub const VERSION: &str = concat!(
     "v",
@@ -16,16 +19,117 @@ pub const VERSION: &str = concat!(
     env!("VERGEN_BUILD_DATE")
 );
 
-const FAILED_TO_SET_ICON: &str = "Failed to set icon";
-
-#[cfg(debug_assertions)]
-const FAILED_TO_SET_TOOLTIP: &str = "Failed to set utilization tooltip";
-
 #[allow(dead_code)]
 #[derive(Debug)]
 enum UserEvent {
     TrayIconEvent(TrayIconEvent),
     MenuEvent(MenuEvent),
+}
+
+struct Application {
+    quit_menu_item: MenuItem,
+    sys: RefCell<System>,
+    tray_icon: TrayIcon,
+}
+
+impl Application {
+    fn new() -> Self {
+        let quit_menu_item = MenuItem::new("Quit", true, None);
+
+        let tray_menu = Menu::new();
+        tray_menu
+            .append_items(&[
+                &PredefinedMenuItem::about(
+                    None,
+                    Some(AboutMetadata {
+                        name: Some("doom-status".to_string()),
+                        copyright: Some("Copyright rjzak".to_string()),
+                        version: Some(VERSION.to_string()),
+                        short_version: Some(env!("CARGO_PKG_VERSION").to_string()),
+                        icon: Some(assets::icon_zero_muda()),
+                        ..Default::default()
+                    }),
+                ),
+                &PredefinedMenuItem::separator(),
+                &quit_menu_item,
+            ])
+            .expect("Failed to create menu");
+
+        let tray_icon = TrayIconBuilder::new()
+            .with_menu(Box::new(tray_menu))
+            .with_icon(assets::load_icon(0))
+            .build()
+            .expect("Failed to create tray icon object");
+
+        let app = Self {
+            quit_menu_item,
+            sys: RefCell::new(System::new_all()),
+            tray_icon,
+        };
+
+        app.update();
+        app
+    }
+
+    pub fn update(&self) {
+        self.sys.borrow_mut().refresh_cpu_all();
+        let usage = self.sys.borrow().global_cpu_usage();
+
+        // We create the icon once the event loop is actually running
+        // to prevent issues like https://github.com/tauri-apps/tray-icon/issues/90
+        self.tray_icon
+            .set_icon(Some(assets::load_icon(usage as u8)))
+            .expect("Failed to set icon");
+
+        #[cfg(debug_assertions)]
+        println!("CPU usage: {:.1}%", usage);
+    }
+}
+
+impl ApplicationHandler<UserEvent> for Application {
+    fn new_events(&mut self, _event_loop: &ActiveEventLoop, cause: StartCause) {
+        if cause == StartCause::Init {
+            self.update();
+
+            // We have to request a redraw here to have the icon actually show up.
+            // Winit only exposes a redraw method on the Window so we use core-foundation directly.
+            #[cfg(target_os = "macos")]
+            unsafe {
+                use core_foundation::runloop::{CFRunLoopGetMain, CFRunLoopWakeUp};
+
+                let rl = CFRunLoopGetMain();
+                CFRunLoopWakeUp(rl);
+            }
+        }
+
+        if cause == StartCause::Poll {
+            self.update();
+        }
+    }
+
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {
+        self.update()
+    }
+
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, event: UserEvent) {
+        if let UserEvent::MenuEvent(event) = event {
+            if event.id == self.quit_menu_item.id() {
+                std::process::exit(0);
+            }
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        _event: WindowEvent,
+    ) {
+    }
+
+    fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
+        self.update();
+    }
 }
 
 fn main() {
@@ -59,106 +163,14 @@ fn main() {
     });
 
     let event_loop = EventLoop::<UserEvent>::with_user_event().build().unwrap();
-
+    event_loop.set_control_flow(ControlFlow::wait_duration(std::time::Duration::from_secs(
+        2,
+    )));
     let proxy = event_loop.create_proxy();
     MenuEvent::set_event_handler(Some(move |event| {
         proxy.send_event(UserEvent::MenuEvent(event)).unwrap();
     }));
 
-    let quit_menu_item = MenuItem::new("Quit", true, None);
-    let tray_menu = Menu::new();
-    tray_menu
-        .append_items(&[
-            &PredefinedMenuItem::about(
-                None,
-                Some(AboutMetadata {
-                    name: Some("doom-status".to_string()),
-                    copyright: Some("Copyright rjzak".to_string()),
-                    version: Some(VERSION.to_string()),
-                    short_version: Some(env!("CARGO_PKG_VERSION").to_string()),
-                    icon: Some(assets::icon_zero_muda()),
-                    ..Default::default()
-                }),
-            ),
-            &PredefinedMenuItem::separator(),
-            &quit_menu_item,
-        ])
-        .expect("Failed to create menu");
-
-    let mut sys = System::new_all();
-    let usage = sys.global_cpu_usage();
-
-    let tray_icon = TrayIconBuilder::new()
-        .with_menu(Box::new(tray_menu))
-        .with_icon(load_icon(usage as u8))
-        .with_tooltip(format!("CPU usage: {:.1}%", usage))
-        .build()
-        .expect("Failed to create tray icon object");
-
-    event_loop
-        .run(move |event, event_loop| {
-            event_loop.set_control_flow(ControlFlow::wait_duration(
-                std::time::Duration::from_secs(2),
-            ));
-
-            match event {
-                #[cfg(not(target_os = "linux"))]
-                Event::NewEvents(winit::event::StartCause::Init) => {
-                    sys.refresh_cpu_all();
-                    let usage = sys.global_cpu_usage();
-
-                    // We create the icon once the event loop is actually running
-                    // to prevent issues like https://github.com/tauri-apps/tray-icon/issues/90
-                    tray_icon
-                        .set_icon(Some(load_icon(usage as u8)))
-                        .expect(FAILED_TO_SET_ICON);
-
-                    // This causes flashing on some platforms
-                    #[cfg(debug_assertions)]
-                    tray_icon
-                        .set_tooltip(Some(format!("CPU usage: {:.1}%", usage)))
-                        .expect(FAILED_TO_SET_TOOLTIP);
-
-                    // We have to request a redraw here to have the icon actually show up.
-                    // Winit only exposes a redraw method on the Window so we use core-foundation directly.
-                    #[cfg(target_os = "macos")]
-                    unsafe {
-                        use core_foundation::runloop::{CFRunLoopGetMain, CFRunLoopWakeUp};
-
-                        let rl = CFRunLoopGetMain();
-                        CFRunLoopWakeUp(rl);
-                    }
-                }
-
-                Event::UserEvent(UserEvent::TrayIconEvent(event)) => {
-                    println!("{event:?}");
-                }
-
-                Event::UserEvent(UserEvent::MenuEvent(event)) => {
-                    if event.id == quit_menu_item.id() {
-                        std::process::exit(0);
-                    }
-                }
-
-                Event::AboutToWait => {
-                    sys.refresh_cpu_all();
-                    let usage = sys.global_cpu_usage();
-
-                    // We create the icon once the event loop is actually running
-                    // to prevent issues like https://github.com/tauri-apps/tray-icon/issues/90
-                    tray_icon
-                        .set_icon(Some(load_icon(usage as u8)))
-                        .expect(FAILED_TO_SET_ICON);
-
-                    // This causes flashing on some platforms
-                    #[cfg(debug_assertions)]
-                    tray_icon
-                        .set_tooltip(Some(format!("CPU usage: {:.1}%", usage)))
-                        .expect(FAILED_TO_SET_TOOLTIP);
-                }
-
-                _ => {}
-            }
-        })
-        .unwrap()
+    let mut app = Application::new();
+    event_loop.run_app(&mut app).unwrap();
 }
