124127ea0fc2c6e06ac9d292ca26cf15d750426c
Richard Zak <richard.j.zak@gmail.com>
2024-12-29 20:28:12 -0500

    feat: icon for Windows
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
 use vergen_gitcl::{BuildBuilder, Emitter, GitclBuilder};
 
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("assets/1.ico");
+        res.compile().unwrap();
+    }
+
     let mut git = GitclBuilder::default();
     let git = git.all().describe(false, true, None).build().unwrap();
 
